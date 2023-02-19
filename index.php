268affa1b823cc2ea9e62711a2a79eee495419bc
michalaforest <michalaforest@hotmail.com>
2023-02-19 17:19:32 -0500

    Started mobile nav

--- index.php
+++ index.php
@@ -16,18 +16,20 @@
     <script src="https://kit.fontawesome.com/36e897625c.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header><?php include './header.php' ?></header>
-    <main>
-        <!-- recipe book -->
-        <section class="recipe-book">
-            <img src="img/gold-paperclip.png" class="gold-paperclip" alt="gold paperclip">
-            <div class="title-container">
-                <img src="img/lemon-tape.png" class="lemon-tape" alt="lemon print washi tape">
-                <h1 class="home-title">My Recipes</h1>
-                <img src="img/lemon-tape.png" class="lemon-tape" alt="lemon print washi tape">
-            </div>
-        </section>
-    </main>
+    <div class="mobile-container">
+        <header><?php include './header.php' ?></header>
+        <main>
+            <!-- recipe book -->
+            <section class="recipe-book">
+                <img src="img/gold-paperclip.png" class="gold-paperclip" alt="gold paperclip">
+                <div class="title-container">
+                    <img src="img/lemon-tape.png" class="lemon-tape" alt="lemon print washi tape">
+                    <h1 class="home-title">My Recipes</h1>
+                    <img src="img/lemon-tape.png" class="lemon-tape" alt="lemon print washi tape">
+                </div>
+            </section>
+        </main>
+    </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" defer></script>
     <script src="js/main.js" defer></script>
 </body>
