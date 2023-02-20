bd06d7258b21ec7ff108c70f1f7c306b5ca49b81
michalaforest <michalaforest@hotmail.com>
2023-02-19 19:50:33 -0500

    Finished mobile nav, and disabled hover effect on active nav tab

--- header.php
+++ header.php
@@ -1,26 +1,15 @@
 <!-- category tabs -->
 <nav class="tabs">
     <ul>
-        <li id="add-new-tab"><a href="add-new.php">Add New!</a></li>
-        <li id="index-tab"><a href="index.php"><i class="fa-solid fa-house"></i></a></li>
-        <li id="apps-tab"><a href="apps.php">Apps</a></li>
-        <li id="mains-tab"><a href="main.php">Mains</a></li>
-        <li id="sides-tab"><a href="sides.php">Sides</a></li>
-        <li id="desserts-tab"><a href="desserts.php">Desserts</a></li>
-        <li id="drinks-tab"><a href="drinks.php">Drinks</a></li>
-        <li id="sauces-tab"><a href="sauces.php">Sauces</a></li>
+        <li id="add-new-tab" class="hover-tab0"><a href="add-new.php">Add New!</a></li>
+        <li id="index-tab" class="hover-tab1"><a href="index.php"><i class="fa-solid fa-house"></i></a></li>
+        <li id="apps-tab" class="hover-tab2"><a href="apps.php">Apps</a></li>
+        <li id="mains-tab" class="hover-tab3"><a href="main.php">Mains</a></li>
+        <li id="sides-tab" class="hover-tab4"><a href="sides.php">Sides</a></li>
+        <li id="desserts-tab" class="hover-tab5"><a href="desserts.php">Desserts</a></li>
+        <li id="drinks-tab" class="hover-tab6"><a href="drinks.php">Drinks</a></li>
+        <li id="sauces-tab" class="hover-tab7"><a href="sauces.php">Sauces</a></li>
     </ul>
 </nav>
 
-<nav class="tabs-mobile">
-    <ul>
-        <li id="add-new-tab"><a href="add-new.php">Add New!</a></li>
-        <li id="index-tab"><a href="index.php"><i class="fa-solid fa-house"></i></a></li>
-        <li id="apps-tab"><a href="apps.php">Apps</a></li>
-        <li id="mains-tab"><a href="main.php">Mains</a></li>
-        <li id="sides-tab"><a href="sides.php">Sides</a></li>
-        <li id="desserts-tab"><a href="desserts.php">Desserts</a></li>
-        <li id="drinks-tab"><a href="drinks.php">Drinks</a></li>
-        <li id="sauces-tab"><a href="sauces.php">Sauces</a></li>
-    </ul>
-</nav>
+
--- js/main.js
+++ js/main.js
@@ -9,10 +9,14 @@ let pageLinks = document.links;
 // cycle through all the tab href links
 for (var i = 0; i < pageLinks.length; i++)
 {
-    // if the tab's href matched the current (active) href, remove hover effect
+    // if the tab's href matched the current (active) href, remove hover effects
     if(pageLinks[i].href == window.location.href) {
 
+        // remove moving tab on hover
         pageLinks[i].parentElement.classList.add("current-tab");
+        // remove darker color on hover
+        pageLinks[i].parentElement.classList.remove("hover-tab" + i);
+
     }
 
 }
