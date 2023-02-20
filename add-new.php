32c7192effa1c579c4187d8f9657dd8f3c50b75e
michalaforest <michalaforest@hotmail.com>
2023-02-20 17:51:23 -0500

    Completed desktop styling for add-new form

--- add-new.php
+++ add-new.php
@@ -16,83 +16,86 @@
     <script src="https://kit.fontawesome.com/36e897625c.js" crossorigin="anonymous"></script>
 </head>
 <body>
-<header><?php include './header.php' ?></header>
-
-<main>
-    <section>
-        <h1 class="add-new-title">Add New!</h1>
-        <form class="add-new-form" action="save-recipe.php" method="POST">
-           <!-- <fieldset>
-                <div>
-                    <label for="upload">Upload image:</label>
+    <div class="mobile-container">
+        <header><?php include './header.php' ?></header>
+        <main>
+            <!-- recipe book -->
+            <section class="recipe-book">
+                <img src="img/gold-paperclip.png" class="gold-paperclip" alt="gold paperclip">
+                <div class="title-container">
+                    <h1 class="add-new-title">Add New!</h1>
                 </div>
-                <input name="upload-image" id="upload-image" type="file" placeholder="Choose image"/>
-            </fieldset> -->
-            <fieldset>
-                <div>
-                    <label for="name">Name:</label>
-                </div>
-                <input name="name" id="name"/>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label for="category">Category:</label>
-                </div>
-                <select name="category" id="category">
-                    <option value="" disabled selected>Categories</option>
-                    <option value="app">Apps</option>
-                    <option value="main">Mains</option>
-                    <option value="side">Sides</option>
-                    <option value="dessert">Desserts</option>
-                    <option value="drink">Drinks</option>
-                    <option value="sauce">Sauces</option>
-                </select>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label for="servings">Servings:</label>
-                </div>
-                <input name="servings" id="servings" type="number"/>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label for="prep-time">Prep time (minutes):</label>
-                </div>
-                <input name="prep-time" id="prep-time" type="number"/>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label for="cook-time">Cook time (minutes):</label>
-                </div>
-                <input name="cook-time" id="cook-time" type="number"/>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label for="rating">Rating (0-5):</label>
-                </div>
-                <input name="rating" id="rating" type="number"/>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label for="ingredients">Ingredients:</label>
-                </div>
-                <textarea name="ingredients" id="ingredients"></textarea>
-            </fieldset>
-            <fieldset>
-                <div>
-                    <label for="directions">Directions:</label>
-                </div>
-                <textarea name="directions" id="directions"></textarea>
-            </fieldset>
-            <div>
-                <button>Save</button>
-            </div>
-        </form>
-    </section>
-</main>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" defer></script>
-<script src="js/main.js" defer></script>
-
+                <form class="add-new-form" action="save-recipe.php" method="POST">
+                    <fieldset>
+                         <div>
+                             <label for="upload">Upload image:</label>
+                         </div>
+                         <input name="upload-image" id="upload-image" type="file"/>
+                     </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="name">Recipe name:</label>
+                        </div>
+                        <input name="name" id="name"/>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="category">Category:</label>
+                        </div>
+                        <select name="category" id="category">
+                            <option value="" disabled selected>Categories</option>
+                            <option value="app">Apps</option>
+                            <option value="main">Mains</option>
+                            <option value="side">Sides</option>
+                            <option value="dessert">Desserts</option>
+                            <option value="drink">Drinks</option>
+                            <option value="sauce">Sauces</option>
+                        </select>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="servings">Servings:</label>
+                        </div>
+                        <input name="servings" id="servings" type="number"/>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="prep-time">Prep time (minutes):</label>
+                        </div>
+                        <input name="prep-time" id="prep-time" type="number"/>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="cook-time">Cook time (minutes):</label>
+                        </div>
+                        <input name="cook-time" id="cook-time" type="number"/>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="rating">Rating (0-5):</label>
+                        </div>
+                        <input name="rating" id="rating" type="number"/>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="ingredients">Ingredients:</label>
+                        </div>
+                        <textarea name="ingredients" id="ingredients"></textarea>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label for="directions">Directions:</label>
+                        </div>
+                        <textarea name="directions" id="directions"></textarea>
+                    </fieldset>
+                    <div>
+                        <button>Save</button>
+                    </div>
+                </form>
+            </section>
+        </main>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" defer></script>
+    <script src="js/main.js" defer></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ for (var i = 0; i < pageLinks.length; i++)
     // if the tab's href matched the current (active) href, remove hover effects
     if(pageLinks[i].href == window.location.href) {
 
-        // remove moving tab on hover
+        // remove moving tab and cursor pointer on hover, and bring tab to the front of the page
         pageLinks[i].parentElement.classList.add("current-tab");
         // remove darker color on hover
         pageLinks[i].parentElement.classList.remove("hover-tab" + i);
